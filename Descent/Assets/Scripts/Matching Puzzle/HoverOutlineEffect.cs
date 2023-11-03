d25ba126ccca1405381693c535c0d80cb06cde49
Bilal Khan <therealbills@hotmail.com>
2023-11-03 01:47:17 -0400

    stuff

---
+++ Descent/Assets/Scripts/Matching Puzzle/HoverOutlineEffect.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class HoverOutlineEffect : MonoBehaviour
+{
+    [SerializeField] Material outlineMaterial; 
+    Material originalMaterial; 
+    Renderer objectRenderer;
+
+    void Awake()
+    {
+        objectRenderer = GetComponent<Renderer>();
+        originalMaterial = objectRenderer.material;
+    }
+
+    void OnMouseEnter() => objectRenderer.material = outlineMaterial;
+
+    void OnMouseExit() => objectRenderer.material = originalMaterial;
+}
