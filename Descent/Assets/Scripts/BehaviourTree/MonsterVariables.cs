051ef224fdf12e7b7e3815930b94da31bd37f977
Bilal <86686062+Bilal-A-G@users.noreply.github.com>
2023-12-29 13:13:00 -0500

    Made it so the navmesh is not entirely in the level and also added wandering, but ran into a huge bug where the monster's IK system is incredibly unstable, eventually resulting in it looking really bad

--- Descent/Assets/Scripts/BehaviourTree/MonsterVariables.cs
+++ Descent/Assets/Scripts/BehaviourTree/MonsterVariables.cs
@@ -7,6 +7,7 @@ public class MonsterVariables : MonoBehaviour
     public float alertness;
     public string playerName;
     public Transform player;
+    public Transform spider;
 
     public Vector3 playerLastKnownPos;
     public float alertnessDecreaseRate;
--- Descent/Assets/Scripts/BehaviourTree/Nodes/Wander.cs
+++ Descent/Assets/Scripts/BehaviourTree/Nodes/Wander.cs
@@ -1,24 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class Wander : NodeBase
 {
     public MonsterMovementController controller;
     public MonsterVariables variables;
+    public float horizontalRotation;
+    public float verticalRotation;
+    public List<Transform> waypoints;
 
-    public override void OnTransition()
-    {
-        return;
-    }
+    public float debugSphereRadius;
+    public Color debugColor;
+    private Transform m_currentWaypoint;
+    private int m_currentWaypointIndex = -1;
 
-    public override void OnExit()
-    {
-        return;
-    }
+    private Vector3 m_wanderPoint;
 
     public override void Tick()
     {
-        controller.Move(variables.playerLastKnownPos);
+        if (controller.ReachedTarget())
+        {
+            m_currentWaypointIndex++;
+            if (m_currentWaypointIndex >= waypoints.Count)
+                m_currentWaypointIndex = 0;
+        }
+
+        m_currentWaypoint = waypoints[m_currentWaypointIndex];
+
+        RaycastHit hit;
+        float rotationXZ = Random.Range(-horizontalRotation, horizontalRotation);
+        float rotationYZ = Random.Range(-verticalRotation, verticalRotation);
+
+        Vector3 raycastDirectionXZ = Quaternion.AngleAxis(rotationXZ, m_currentWaypoint.up) * m_currentWaypoint.forward;
+        Vector3 finalRaycastDirection = Quaternion.AngleAxis(rotationYZ, m_currentWaypoint.right) * raycastDirectionXZ;
+
+        if (Physics.Raycast(m_currentWaypoint.position, finalRaycastDirection.normalized, out hit))
+        {
+            controller.Move(hit.point);
+            m_wanderPoint = hit.point;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (m_wanderPoint == controller.runner.transform.position)
+            return;
+
+        Gizmos.color = debugColor;
+        Gizmos.DrawSphere(m_wanderPoint, debugSphereRadius);
     }
 }
