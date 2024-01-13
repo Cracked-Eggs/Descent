aad1b02c0b53ce0467107a1684480721670d7752
Bilal <86686062+Bilal-A-G@users.noreply.github.com>
2024-01-13 18:27:05 -0500

    Added hearing via event system so anything in game can trigger it, even prefabs

--- Descent/Assets/Scripts/BehaviourTree/Senses/Hearing.cs
+++ Descent/Assets/Scripts/BehaviourTree/Senses/Hearing.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class Hearing : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public EventObject madeQuietSound;
+    public EventObject madeMediumSound;
+    public EventObject madeLoudSound;
+
+    public EventObject playerSpotted;
+
+    public float quietNoiseAlertness;
+    public float mediumNoiseAlertness;
+    public float loudNoiseAlertness;
+
+    public float noiseFalloff;
+
+    public MonsterVariables variables;
+
+    private bool m_playerSpotted;
+
+    private void Start()
     {
-        
+        playerSpotted.Subscribe((bool spotted) => {m_playerSpotted = spotted;});
+
+        madeQuietSound.Subscribe((bool _) =>
+        {
+            Debug.Log("Made quiet sound");
+            //Temporary, change it so the monster can hear sounds that do not originate from the player
+            AlertMonster(quietNoiseAlertness, variables.player.position);
+        });
+
+        madeMediumSound.Subscribe((bool _) => 
+        {
+            Debug.Log("Made medium sound");
+            AlertMonster(mediumNoiseAlertness, variables.player.position);
+        });
+
+        madeLoudSound.Subscribe((bool _) => 
+        {
+            Debug.Log("Made loud sound");
+            AlertMonster(loudNoiseAlertness, variables.player.position);
+        });
     }
 
-    // Update is called once per frame
-    void Update()
+    private void AlertMonster(float amount, Vector3 soundPosition)
     {
-        
+        if (m_playerSpotted)
+            return;
+
+        float distanceToMonster = (soundPosition - variables.spider.position).magnitude;
+        float attentuatedAlertness = amount - distanceToMonster * noiseFalloff;
+        if (attentuatedAlertness < 0)
+            attentuatedAlertness = 0;
+
+        variables.alertness += attentuatedAlertness;
+        variables.playerLastKnownPos = soundPosition;
+        Debug.Log(variables.alertness);
     }
 }
--- Descent/Assets/Scripts/Player/PlayerController.cs
+++ Descent/Assets/Scripts/Player/PlayerController.cs
@@ -16,6 +16,9 @@ public class PlayerController : MonoBehaviour
     public float jumpForce;
     public float jumpBuffering;
 
+    public EventObject madeQuietNoise;
+    public EventObject madeMediumNoise;
+
     private Vector2 m_movementDirection;
     private Vector2 m_mouseDelta;
     private float m_gravityForce;
@@ -30,7 +33,18 @@ public class PlayerController : MonoBehaviour
         actions = new InputActions();
         actions.Enable();
 
-        actions.Default.Movement.performed += ctx => m_movementDirection = ctx.ReadValue<Vector2>().normalized;
+        actions.Default.Movement.performed += ctx => 
+        {
+            m_movementDirection = ctx.ReadValue<Vector2>().normalized;
+            if (m_running)
+            {
+                madeMediumNoise.Invoke(true);
+            }
+            else
+            {
+                madeQuietNoise.Invoke(true);
+            }
+        };
         actions.Default.Look.performed += ctx => m_mouseDelta = ctx.ReadValue<Vector2>();
         actions.Default.Jump.performed += ctx =>
         {
@@ -45,6 +59,7 @@ public class PlayerController : MonoBehaviour
             }
 
             m_lastJumpTime = Time.time;
+            madeMediumNoise.Invoke(true);
         };
         actions.Default.Run.performed += ctx => m_running = true;
         actions.Default.Run.canceled += ctx => m_running = false;
